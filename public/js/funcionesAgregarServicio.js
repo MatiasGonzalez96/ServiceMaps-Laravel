7b81ccb27413ffefe803212e384c3d961126f580
Matias Gonzalez <matialem8@gmail.com>
2018-05-17 14:15:55 -0300

    Creación del formulario para agregar servicios

--- public/js/funcionesAgregarServicio.js
+++ public/js/funcionesAgregarServicio.js
@@ -5,3 +5,19 @@ $(function()
         window.location.href = "/";
     });
 });
+
+$(function()
+{
+    $("#botonAgregarServicio").click(function()
+    {
+        validateForm();
+    });
+});
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
